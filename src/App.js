253855909d7fe795ae6872134b91f03d34353e20
Ishant Lambhate <ishantlambhate@gmail.com>
2018-09-02 22:26:01 +0530

    Finishing Built

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movies from './Movies';
-import Search from './Search';
-import SortBy from './SortBy';
+
 
 class App extends Component {
 
@@ -11,54 +10,16 @@ class App extends Component {
     
     this.state = {
       movies: [],
-      filterMovies: []
+      filterMovies:[]
     };
-    
-    this.onInput = this.onInput.bind(this);
-    this.onSort = this.onSort.bind(this);
-  }
-  
-  onInput(query) {
-    let filterData =  this.state.movies.filter((obje) => obje.movie_title.toLowerCase().startsWith(query.toLowerCase()));
-    this.setState({filterMovies: filterData});
-  }
-  
-  onSort (value) {
-    let sortData;
-    if(value==='title_year'){
-      sortData = this.state.movies.sort((a, b) => a.title_year - b.title_year);
-      this.setState({filterMovies: sortData}); 
-    }else if(value==='content_rating'){
-      sortData = this.state.movies.sort((a, b) => a.content_rating > b.content_rating);
-      this.setState({filterMovies: sortData}); 
-    }
-
   }
 
-  getPopularMovies() {
-    const url = `http://starlord.hackerearth.com/movieslisting`;
-    fetch (url)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          movies: data,
-          filterMovies: data
-        })
-      });
-  }
-    
-  componentDidMount() {
-    this.getPopularMovies();  
-  }
-  
+ 
   render() {
-    const { movies, filterMovies } = this.state;
-  
+    
     return (
       <div className="app">
-        <Search onInput={this.onInput} placeholder="Search for Movie Title …" />
-        <SortBy onSort={this.onSort}/>
-        <Movies movies={filterMovies} />
+        <Movies />
       </div>
     );
   }
--- src/Movies.js
+++ src/Movies.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Movie from './Movie';
+import Search from './Search';
+import SortBy from './SortBy';
 
 class Movies extends Component {
 
@@ -10,24 +12,66 @@ class Movies extends Component {
     this.state = {
           todos: '',
           currentPage: 1,
-          todosPerPage: 20
+          todosPerPage: 20,
+          movies: [],
+          filterMovies: []
         };
         this.handleClick = this.handleClick.bind(this);
-    
+        this.onInput = this.onInput.bind(this);
+        this.onSort = this.onSort.bind(this);
     }
+
+      onInput(query) {
+    let filterData =  this.state.movies.filter((obje) => obje.movie_title.toLowerCase().startsWith(query.toLowerCase()));
+    this.setState({filterMovies: filterData,
+                currentPage:1  
+    });
+  }
+  
+  onSort (value) {
+    let sortData;
+    if(value==='title_year'){
+      sortData = this.state.movies.sort((a, b) => a.title_year - b.title_year);
+      this.setState({filterMovies: sortData,
+                    currentPage:1  
+      }); 
+    }else if(value==='content_rating'){
+      sortData = this.state.movies.sort((a, b) => a.content_rating > b.content_rating);
+      this.setState({filterMovies: sortData,
+                     currentPage:1  }); 
+    }
+
+  }
+
  handleClick(event) {
         this.setState({
           currentPage: Number(event.target.id)
         });
       }
 
+       getPopularMovies() {
+    const url = `http://starlord.hackerearth.com/movieslisting`;
+    fetch (url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          movies: data,
+          filterMovies: data
+        })
+      });
+  }
+    
+  componentDidMount() {
+    this.getPopularMovies();  
+  }
+  
 render() {
         const { currentPage, todosPerPage } = this.state;
 
         // Logic for displaying current todos
         const indexOfLastTodo = currentPage * todosPerPage;
         const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
-        const currentTodos = this.props.movies.slice(indexOfFirstTodo, indexOfLastTodo);
+        const currentTodos = this.state.filterMovies.slice(indexOfFirstTodo, indexOfLastTodo);
 
         const renderTodos = currentTodos.map((movie, index) => {
           return <li key={movie.id}>
@@ -37,7 +81,7 @@ render() {
 
         // Logic for displaying page numbers
         const pageNumbers = [];
-        for (let i = 1; i <= Math.ceil(this.props.movies.length / todosPerPage); i++) {
+        for (let i = 1; i <= Math.ceil(this.state.filterMovies.length / todosPerPage); i++) {
           pageNumbers.push(i);
         }
 
@@ -55,6 +99,8 @@ render() {
 
         return (
           <div>
+           <Search onInput={this.onInput} placeholder="Search for Movie Title …" />
+            <SortBy onSort={this.onSort}/>
            <div align="right">Page No: {this.state.currentPage} of {pageNumbers.length}</div>
            <ul className="movies">
             {renderTodos}
